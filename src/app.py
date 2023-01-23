3f8bb8b3d2f331ac81366ce1b85c571471988d5a
jaranedag <jaranedag@udd.cl>
2023-01-23 17:41:31 +0000

    actualizacion backend para login

--- src/app.py
+++ src/app.py
@@ -80,6 +80,40 @@ def login():
         })
       else:
         return "datos incorrectos"  
+@app.route('/user', methods=['GET','POST'])
+def handle_hello():
+    #cuando es un get conseguiremos todos los usuarios 
+    if request.method =='GET':
+        all_people = User.query.all()
+        all_people = list(map(lambda x: x.serialize(), all_people))
+    
+        return jsonify(all_people), 200
+    
+    else:
+        body = request.get_json() # obtener el request body de la solicitud
+        if body is None:
+            return "The request body is null", 400
+        if 'email' not in body:
+            return 'Especificar email', 400
+        if 'password' not in body:
+            return 'Especificar password',400
+        #estoy consultando si existe alguien con el email que mande en la api y consigo la primera coincidencia
+        onePeople = User.query.filter_by(email=body["email"]).first()
+        if onePeople:
+            if (onePeople.password == body["password"] ):
+                #CUANDO VALIDAMOS LA PASSWORD CREAREMOS EL TOKEN
+                expira = datetime.timedelta(minutes=2)
+                access_token = create_access_token(identity=onePeople.email, expires_delta=expira)
+                data = {
+                    "info_user": onePeople.serialize(),
+                    "token": access_token,
+                    "expires": expira.total_seconds()
+                }
+                return(jsonify(data))
+            else:
+                return(jsonify({"mensaje":False}))
+        else:
+            return(jsonify({"mensaje":"mail no se encuentra registrado"}))        
 @app.route("/private",methods=["GET"])
 @jwt_required()
 def privada():
